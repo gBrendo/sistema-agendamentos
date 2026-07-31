5f06f4b177569bb8c9a74a5b479316784f04c898
Brendo Gomes <brendo.carmo@hotmail.com>
2026-07-31 17:24:32 -0300

    TDD: caminho feliz do AgendamentoService (sem conflito)

--- tests/Agendamentos.UnitTests/AgendamentoServiceTests.cs
+++ tests/Agendamentos.UnitTests/AgendamentoServiceTests.cs
@@ -1,5 +1,6 @@
 using Agendamentos.Application.Interfaces;
 using Agendamentos.Application.Services;
+using Agendamentos.Domain.Entities;
 using FluentAssertions;
 using Moq;
 using Xunit;
@@ -26,5 +27,26 @@ public class AgendamentoServiceTests
         // Act + Assert
         await Assert.ThrowsAsync<InvalidOperationException>(() =>
             service.CriarAgendamentoAsync(clienteId, profissionalId, dataHora));
+    }
+    [Fact]
+    public async Task CriarAgendamento_QuandoNaoHaConflito_DeveChamarRepositorioParaAdicionar()
+    {
+        // Arrange
+        var clienteId = Guid.NewGuid();
+        var profissionalId = Guid.NewGuid();
+        var dataHora = DateTime.Now.AddDays(1);
+
+        var repositorioMock = new Mock<IAgendamentoRepository>();
+        repositorioMock
+            .Setup(r => r.ExisteConflitoAsync(profissionalId, dataHora))
+            .ReturnsAsync(false);
+
+        var service = new AgendamentoService(repositorioMock.Object);
+
+        // Act
+        await service.CriarAgendamentoAsync(clienteId, profissionalId, dataHora);
+
+        // Assert
+        repositorioMock.Verify(r => r.AdicionarAsync(It.IsAny<Agendamento>()), Times.Once);
     }
 }
